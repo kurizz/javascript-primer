1482f3e20e8dd26ce3ad1f358b14ef7bec10d814
n-mug <umozon63313@gmail.com>
2021-03-30 23:47:04 +0900

    :fire: .ts はやめて、普通に .js にすることにしたので、削除

--- tostring.ts
+++
@@ -1,8 +0,0 @@
-const obj = {
-    key: "value"
-}
-
-console.log(obj.toString());
-console.log(String(obj)); // String コンストラクタの中で toString() が呼ばれる
-
-console.log(obj.toString() === String(obj)); // これは true になる
