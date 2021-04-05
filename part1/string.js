89ac9a827e520044c20ec2d4f851a2c0e2ee906b
n-mug <umozon63313@gmail.com>
2021-04-05 21:36:24 +0900

    文字列 (続)

--- part1/string.js
+++ part1/string.js
@@ -3,4 +3,16 @@ const multiline = `1 行目
 2 行目
 3 行目`;
 console.log(multiline); // '`' は、改行をそのまま挿入できる
+console.log(`---\n${multiline}\n---`); // ${} は評価時に展開される
 
+/* 文字列とは */
+const str = "アオイ"; // 文字コード: Unicode, エンコード方式: UTF-16 をそれぞれ採用
+console.log(str.charCodeAt(0).toString(16));
+console.log(str.charCodeAt(1).toString(16));
+console.log(str.charCodeAt(2).toString(16));
+
+console.log( // 進数から文字列への変換
+    String.fromCharCode(`0x${str.charCodeAt(0).toString(16)}`),
+    String.fromCharCode(`0x${str.charCodeAt(1).toString(16)}`),
+    String.fromCharCode(`0x${str.charCodeAt(2).toString(16)}`),
+);
